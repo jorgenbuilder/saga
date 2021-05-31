59d233426df1edabd13d6bdde760e2749bf4e8e2
Jorgen <jorgen@buildnode.io>
2021-05-30 21:32:32 -0700

    add dev hook to force certain card draw

--- src/Atoms/draw.ts
+++ src/Atoms/draw.ts
@@ -1,8 +1,12 @@
 import { cardLabels } from '../Assets/cards';
 
 const DrawCard: () => [number, boolean] = () => {
-    const randomSeed = getRandomInt(1, 78);
-    const randomBool = Math.random() >= .66;
+    //@ts-ignore
+    const [FORCED_DRAW, FORCED_ORIENTATION] = window.FORCE_NEXT_DRAW || [undefined, undefined];
+    //@ts-ignore
+    window.FORCE_NEXT_DRAW = undefined;
+    const randomSeed = typeof FORCED_DRAW !== 'undefined' ? FORCED_DRAW : getRandomInt(1, 78);
+    const randomBool = typeof FORCED_ORIENTATION !== 'undefined' ? FORCED_ORIENTATION : Math.random() >= .66;
     const label = cardLabels[randomSeed - 1];
     console.info(`Drew ${label} ${randomBool ? 'up-side-down' : 'right-side-up'} (${randomSeed})`);
     return [randomSeed, randomBool];
