125da6848021cfba696dd6665a373e259373b0b1
Jorgen <jorgen@buildnode.io>
2021-06-30 09:21:39 -0700

    Fix local dev canister calls

--- src/Services/Users.ts
+++ src/Services/Users.ts
@@ -1,20 +1,24 @@
-// import { Actor, HttpAgent } from '@dfinity/agent';
-// import { idlFactory as tarotIdl, canisterId as tarotId } from 'ic:canisters/tarot/tarot';
+import { Actor, HttpAgent } from '@dfinity/agent';
+import { idlFactory as tarotIdl, canisterId as tarotId } from 'ic:canisters/tarot/tarot';
 
 export interface User {
     handle: string;
     tokenBalance: number;
-    isAdmin: boolean;
 }
 
 export const defaultUser: User = {
     handle: '@default',
     tokenBalance: 100,
-    isAdmin: true,
 };
 
-// const agent = new HttpAgent();
-// const tarot = Actor.createActor(tarotIdl, { agent, canisterId: tarotId });
-// tarot.getUser(defaultUser.handle).then(console.log).catch(console.error);
-// tarot.addUser(defaultUser).then(console.log).catch(console.error);
-// tarot.getUser(defaultUser.handle).then(console.log).catch(console.error);
+const agent = new HttpAgent();
+agent.fetchRootKey();  // This is some local development carp
+const tarot = Actor.createActor(tarotIdl, { agent, canisterId: tarotId });
+
+tarot.addUser(defaultUser)
+.then((r) => {
+    console.log(`Response`, r);
+    return tarot.listUsers();
+})
+.then((r) => console.log(`Response`, r))
+.catch((r) => console.error(`Error`, r));
