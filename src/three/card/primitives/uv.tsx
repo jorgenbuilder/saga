81f019186197fdaab6dfc1e8a643d8b37525da7e
Jorgen <jorgen@buildnode.io>
2021-07-03 18:11:19 -0700

    add offset to uv generator

--- src/three/card/primitives/uv.tsx
+++ src/three/card/primitives/uv.tsx
@@ -2,9 +2,12 @@ import * as THREE from 'three';
 import { Shape } from 'three';
 import { getDimensions } from './shapes';
 
-export function CardUVGenerator (shape: Shape) {
+export function CardUVGenerator (shape: Shape, offset = [0, 0, 0, 0]) {
     const [w, h] = getDimensions(shape);
-    const b = [[-w / 2, -h / 2], [+w / 2, +h / 2]];
+    const b = [
+        [-w / 2 + offset[0], -h / 2 + offset[0]],
+        [+w / 2, +h / 2],
+    ];
     return {
         generateTopUV: function (
             geometry: THREE.ExtrudeGeometry,
@@ -22,9 +25,9 @@ export function CardUVGenerator (shape: Shape) {
             const cy = vertices[indexC * 3 + 1];
 
             return [
-                new THREE.Vector2((ax - b[0][0]) / w, (ay - b[0][1]) / h),
-                new THREE.Vector2((bx - b[0][0]) / w, (by - b[0][1]) / h),
-                new THREE.Vector2((cx - b[0][0]) / w, (cy - b[0][1]) / h),
+                new THREE.Vector2((ax - b[0][0]) / (w - (offset[0] + offset[2])), (ay - b[0][1]) / (h - (offset[1] + offset[2]))),
+                new THREE.Vector2((bx - b[0][0]) / (w - (offset[0] + offset[2])), (by - b[0][1]) / (h - (offset[1] + offset[2]))),
+                new THREE.Vector2((cx - b[0][0]) / (w - (offset[0] + offset[2])), (cy - b[0][1]) / (h - (offset[1] + offset[2]))),
             ];
         },
 
