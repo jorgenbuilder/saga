7d4ad96d53e675a79c92577a0c830d0eb38b819b
jorgenbuilder <jorgen@buildnode.io>
2021-07-18 19:50:44 -0700

    remove old svg deck

--- src/assets/cards/index.ts
+++ src/assets/cards/index.ts
@@ -14,28 +14,6 @@ export interface TarotDeckSkin {
         offsetFace?: [number, number],
         paddingFace?: [number, number],
     }
-}
-
-const SvgTarotSkin: TarotDeckSkin = {
-    cards: TarotDeckData.map(card => {
-        const suitMap = {
-            'Trump': 'greater',
-            'Cups': 'lesser-cups',
-            'Swords': 'lesser-swords',
-            'Wands': 'lesser-wands',
-            'Pentacles': 'lesser-pentacles',
-        };
-        const cardNumber = card.suit === 'Trump' ? card.number + 1 : card.number
-        return {
-            cardIndex: card.index,
-            cardName: card.name,
-            filePath: require(`./svg-basic/card-${suitMap[card.suit]}-${cardNumber}.svg`).default,
-        }
-    }).concat([{
-        cardIndex: 78,
-        cardName: 'CARD BACK',
-        filePath: require(`./svg-basic/card-back.svg`).default,
-    }])
 };
 
 const RiderWaiteTarotSkin: TarotDeckSkin = {
@@ -69,6 +47,5 @@ const RiderWaiteTarotSkin: TarotDeckSkin = {
 };
 
 export {
-    SvgTarotSkin,
     RiderWaiteTarotSkin,
 };
