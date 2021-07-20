c16dfa4b8b94f0eec030ea0da52ea2af103f5bcc
Jorgen <jorgen@buildnode.io>
2021-07-19 18:01:53 -0700

    bake the agent host

--- src/constants/agent.ts
+++ src/constants/agent.ts
@@ -3,7 +3,7 @@ import { HttpAgent, HttpAgentOptions } from '@dfinity/agent';
 export default function agent (): HttpAgent {
     const config: HttpAgentOptions = {};
     if (window.location.host.includes('localhost')) {
-        config['host'] = 'http://localhost:8000';
+        config['host'] = 'http://r7inp-6aaaa-aaaaa-aaabq-cai.localhost:8000';
     }
     const agent = new HttpAgent(config);
     if (window.location.host.includes('localhost')) {
