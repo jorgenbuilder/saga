6e89f5938a5b578e068f4d35583ecb8b3525d46a
jorgenbuilder <jorgen@buildnode.io>
2021-07-19 21:38:14 -0700

    handle canister id between local envs

--- src/constants/agent.ts
+++ src/constants/agent.ts
@@ -1,9 +1,10 @@
 import { HttpAgent, HttpAgentOptions } from '@dfinity/agent';
+import { frontendCanisterId } from 'devconf';
 
 export default function agent (): HttpAgent {
     const config: HttpAgentOptions = {};
     if (window.location.host.includes('localhost')) {
-        config['host'] = 'http://r7inp-6aaaa-aaaaa-aaabq-cai.localhost:8000';
+        config['host'] = `http://${frontendCanisterId}.localhost:8000`;
     }
     const agent = new HttpAgent(config);
     if (window.location.host.includes('localhost')) {
